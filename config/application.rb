9b0e019cb9ea52a1792bda3acc484593bd29342c
ayumitamai97 <ayumitamai97@gmail.com>
2020-04-14 16:13:53 +0900

    Hotfix application.rb: YAML load option

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,10 @@ module G3y
     config.api_only = true
 
     # Elasticsearch
-    es_config = YAML.safe_load(ERB.new(File.read('config/elasticsearch.yml')).result)
+    es_config = YAML.safe_load(
+      ERB.new(File.read('config/elasticsearch.yml')).result,
+      aliases: true,
+    )
     es_host = es_config.symbolize_keys[Rails.env.to_sym]['host']
     Elasticsearch::Model.client = Elasticsearch::Client.new(host: es_host)
   end
