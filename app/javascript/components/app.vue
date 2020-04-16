2d5f75dd833e186861fbc4dbfc3de3278414b8c6
ayumitamai97 <ayumitamai97@gmail.com>
2020-04-16 19:27:08 +0900

    Install vue-apollo

--- app/javascript/components/app.vue
+++ app/javascript/components/app.vue
@@ -1,19 +1,38 @@
 <template>
   <div>
-    <p>{{ message }}</p>
+    <p>{{ message }}, {{ userName() }}</p>
+
     <router-link to='/foo'>go to foo</router-link>
     <router-link to='/bar'>go to bar</router-link>
+
     <router-view></router-view>
   </div>
 </template>
 
 <script lang='ts'>
+import gql from 'graphql-tag'
+
 export default {
   data() {
     return {
+      user: '',
       message: 'Hello👋',
     }
   },
+  apollo: {
+    // TODO: ログイン中のユーザ
+    user: gql`query {
+      user(id: 1) {
+        id
+        name
+      }
+    }`,
+  },
+  methods: {
+    userName() {
+      return this.user.name
+    }
+  }
 }
 </script>
 
--- app/javascript/packs/application.ts
+++ app/javascript/packs/application.ts
@@ -2,10 +2,15 @@
 
 import Vue from 'vue/dist/vue.esm'
 import VueRouter from 'vue-router'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import VueApollo from 'vue-apollo'
 import App from '../components/app.vue'
 
 document.addEventListener('DOMContentLoaded', () => {
   Vue.use(VueRouter)
+  Vue.use(VueApollo)
 
   const Foo = { template: '<div>foo</div>' }
   const Bar = { template: '<div>bar</div>' }
@@ -20,10 +25,26 @@ document.addEventListener('DOMContentLoaded', () => {
     routes,
   })
 
+  const httpLink = createHttpLink({
+    // TODO: environmental variable
+    uri: 'http://localhost:3000/graphql',
+  })
+  const cache = new InMemoryCache()
+  const apolloClient = new ApolloClient({
+    link: httpLink,
+    cache,
+  })
+
+  const apolloProvider = new VueApollo({
+    defaultClient: apolloClient,
+  })
+
   new Vue({
     el: '#app',
     components: { App },
     template: '<App/>',
     router,
+    apolloProvider,
+    render: h => h(App),
   })
 })
