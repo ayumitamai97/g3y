ede3e59b93733b315796ce4c94f7a33d65e985d9
ayumitamai97 <ayumitamai97@gmail.com>
2020-04-16 09:40:00 +0900

    Annotate models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  name       :string(255)      not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class User < ApplicationRecord
   include Elasticsearch::Model
 
