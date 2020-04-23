73b6482efd71f63aaba160f549c4b471d4bdfdbb
ayumitamai97 <ayumitamai97@gmail.com>
2020-04-23 13:53:25 +0900

    Update .rubocop.yml

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class RootController < ActionController::Base
+class RootController < ActionController::Base # rubocop:disable Rails/ApplicationController
   layout 'application'
 
   def index; end
