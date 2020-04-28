cc8d50f07259c0a23bd2a28ab2c6892b5cb0b2ec
ayumitamai97 <ayumitamai97@gmail.com>
2020-04-28 17:26:18 +0900

    Hotfix migration file!!: change column order

--- db/migrate/20200424074025_add_columns_to_users.rb
+++ db/migrate/20200424074025_add_columns_to_users.rb
@@ -4,8 +4,8 @@ class AddColumnsToUsers < ActiveRecord::Migration[6.0]
   def change
     change_table :users, bulk: true do |t|
       t.index :name, unique: true
-      t.string :email, null: false, index: { unique: true }, default: ''
-      t.string :password_digest, null: false, default: ''
+      t.string :email, null: false, index: { unique: true }, default: '', after: :name
+      t.string :password_digest, null: false, default: '', after: :email
     end
   end
 end
