af680378d726852f314c4dbb9b396cc8b81832ff
ayumitamai97 <ayumitamai97@gmail.com>
2020-05-01 16:28:44 +0900

    Implement server-side authorization

--- app/javascript/components/app.vue
+++ app/javascript/components/app.vue
@@ -1,14 +1,32 @@
 <template>
   <div>
-    <!-- ↓ TODO: ログアウト時のみ  -->
-    <router-link to="/login">Login</router-link>
-    <router-link to="/signup">Signup</router-link>
+    <div v-if='!isAuthenticated()'>
+      <router-link to="/login">Login</router-link>
+      <router-link to="/signup">Signup</router-link>
+    </div>
+    <div v-if='isAuthenticated()'>
+      <!-- TODO -->
+      <p>Logged in as hogehoge</p>
+      <button v-on:click='logout'>Logout</button>
+    </div>
     <router-view></router-view>
   </div>
 </template>
 
 <script lang='ts'>
+import authUtil from '../src/authUtil.ts'
+
 export default {
   name: 'app',
+  created() {
+    this.isAuthenticated = authUtil.isAuthenticated
+  },
+  methods: {
+    logout(): void {
+      localStorage.removeItem('access')
+      localStorage.removeItem('accessExpiresAt')
+      this.$router.push('/login')
+    },
+  },
 }
 </script>
--- app/javascript/packs/application.ts
+++ app/javascript/packs/application.ts
@@ -4,6 +4,7 @@ import Vue from 'vue/dist/vue.esm'
 import VueRouter from 'vue-router'
 import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
+import { setContext } from 'apollo-link-context'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import VueApollo from 'vue-apollo'
 import InfiniteLoading from 'vue-infinite-loading'
@@ -25,9 +26,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const router = new VueRouter({ mode: 'history', routes })
 
+  // Apollo
+  // ref. https://github.com/vuejs/vue-apollo/issues/144#issuecomment-522945282
   const httpLink = createHttpLink({ uri: `${process.env.HOST}/graphql` })
+  const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem('access')
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : '',
+      },
+    }
+  })
   const cache = new InMemoryCache()
-  const apolloClient = new ApolloClient({ link: httpLink, cache })
+  const apolloClient = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache,
+  })
   const apolloProvider = new VueApollo({ defaultClient: apolloClient })
 
   new Vue({
